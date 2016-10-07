55994fbeb83ee1d2fd8eff0bff1cac17ab8b80fa
Zhitao Gong <zhitaao.gong@gmail.com>
2016-10-06 21:17:03 -0500

    leetcode 004 kth smallest O(log(min(m,n,k)))

--- leetcode/004-median-of-two-sorted-arrays.cpp
+++ leetcode/004-median-of-two-sorted-arrays.cpp
@@ -1,17 +1,23 @@
 class Solution {
  public:
-  int findkth(vector<int>& A, int a, vector<int>& B, int b, int kth)
+  int kth_element(int k, vector<int>& A, vector<int>& B)
   {
-    int dummy = numeric_limits<int>::max();
-    for (int i = a, j = b, k = kth; /* empty */;k -= k / 2) {
-      if (i >= A.size()) return B[j + k - 1];
-      if (j >= B.size()) return A[i + k - 1];
-      if (1 == k) return min(A[i], B[j]);
-      int ka = i + k / 2 - 1 < A.size() ? A[i + k / 2 - 1] : dummy;
-      int kb = j + k / 2 - 1 < B.size() ? B[j + k / 2 - 1] : dummy;
-      if (ka < kb) i += k / 2;
-      else j += k / 2;
+    if (A.size() < B.size()) return kth_element(k, B, A);
+    if (k >= A.size() + B.size()) return max(A.back(), B.back());
+    int imin = numeric_limits<int>::min(),
+        imax = numeric_limits<int>::max();
+    for (int l = 0, h = min(int(B.size() - 1), k); l <= h; ) {
+      int m2 = l + (h - l) / 2;
+      int m1 = k - m2;
+      int L1 = m1 > 0 ? A[m1 - 1] : imin;
+      int R1 = m1 < A.size() ? A[m1] : imax;
+      int L2 = m2 > 0 ? B[m2 - 1] : imin;
+      int R2 = B[m2];
+      if (L1 > R2) l = m2 + 1;
+      else if (L2 > R1) h = m2 - 1;
+      else return min(R1, R2);
     }
+    return A[k - B.size()];
   }
 
   double findMedianSortedArrays(vector<int>& nums1,
@@ -19,8 +25,8 @@ class Solution {
   {
     int len = nums1.size() + nums2.size();
     if (len & 1)
-      return findkth(nums1, 0, nums2, 0, len / 2 + 1);
-    return (findkth(nums1, 0, nums2, 0, len / 2) +
-            findkth(nums1, 0, nums2, 0, len / 2 + 1)) / 2.0;
+      return kth_element(len / 2, nums1, nums2);
+    return (kth_element(len / 2 - 1, nums1, nums2) +
+            kth_element(len / 2, nums1, nums2)) / 2.0;
   }
 };
