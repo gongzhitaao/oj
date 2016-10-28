b5c216d307fe564279bec500ed225681dc524c2a
Zhitao Gong <zhitaao.gong@gmail.com>
2016-10-28 13:41:41 -0500

    leetcode 280 simpler

--- leetcode/280-wiggle-sort.cpp
+++ leetcode/280-wiggle-sort.cpp
@@ -4,15 +4,9 @@ class Solution
   void wiggleSort(vector<int>& nums)
   {
     int n = nums.size();
-    if (n < 2) return;
-
-    nth_element(nums.begin(), nums.begin() + n/2, nums.end());
-    int median = nums[n/2];
-    for (int i = 0, j = 1; i < n && j < n; ) {
-      if (nums[i] >= median) {
-        swap(nums[i], nums[j]);
-        j += 2;
-      } else i += 2;
-    }
+    for (int i = 0; i < n - 1; ++i)
+      if (i%2 && nums[i] < nums[i+1] ||
+          !(i%2) && nums[i] > nums[i+1])
+        swap(nums[i], nums[i+1]);
   }
 };
