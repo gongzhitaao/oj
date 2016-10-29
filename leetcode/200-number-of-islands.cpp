497f6d35a29a11ca496dcfc3d20da1cc1f565141
Zhitao Gong <zhitaao.gong@gmail.com>
2016-10-29 10:29:35 -0500

    leetcode 200 bfs

--- leetcode/200-number-of-islands.cpp
+++ leetcode/200-number-of-islands.cpp
@@ -6,35 +6,28 @@ class Solution
     if (grid.empty() || grid[0].empty()) return 0;
 
     int nrow = grid.size(), ncol = grid[0].size();
-
-    typedef pair<int, int> id;
-
-    set<id> lands;
-    for (int i = 0; i < nrow; ++i)
-      for (int j = 0; j < ncol; ++j)
-        if ('1' == grid[i][j]) lands.insert({i, j});
-
     const vector<int> step{-1, 0, 1, 0, -1};
 
+    typedef pair<int, int> id;
     int cnt = 0;
-    for (; !lands.empty(); ++cnt) {
-      id cur = *lands.begin(); lands.erase(cur);
-      queue<id> q;
-      q.push(cur);
-      while (!q.empty()) {
-        id p = q.front(); q.pop();
-        for (int i = 0; i < 4; ++i) {
-          int r = p.first + step[i], c = p.second + step[i+1];
-          if (0 <= r && r < nrow && 0 <= c && c < ncol &&
-              '1' == grid[r][c] &&
-              lands.find({r, c}) != lands.end()) {
-            lands.erase({r, c});
-            q.push({r, c});
+    queue<id> q;
+    for (int i = 0; i < nrow; ++i) {
+      for (int j = 0; j < ncol; ++j) {
+        if ('0' == grid[i][j]) continue;
+        ++cnt;
+        for (q.push({i, j}); !q.empty(); ) {
+          id p = q.front(); q.pop();
+          for (int k = 0; k < 4; ++k) {
+            int r = p.first + step[k], c = p.second + step[k+1];
+            if (0 <= r && r < nrow && 0 <= c && c < ncol &&
+                '1' == grid[r][c]) {
+              grid[r][c] = '0';
+              q.push({r, c});
+            }
           }
         }
       }
     }
-
     return cnt;
   }
 };
