2355f4ec3f0c429b7b0c0b80ff043b8a761be4dc
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-02 14:44:05 -0500

    leetcode 399 union-find

--- leetcode/399-evaluate-division.cpp
+++ leetcode/399-evaluate-division.cpp
@@ -1,40 +1,75 @@
 class Solution
 {
  public:
+  struct info {
+    string par;
+    double val;
+    int rank;
+  };
+
   vector<double>
   calcEquation(vector<pair<string, string> > equations,
                vector<double>& values,
                vector<pair<string, string> > queries)
   {
-    unordered_map<string, unordered_map<string, double> > G;
     unordered_set<string> V;
+    unordered_map<string, info> myset;
     int n = values.size();
     for (int i = 0; i < n; ++i) {
       pair<string, string>& eq = equations[i];
       double val = values[i];
-      G[eq.first][eq.first] = 1.0;
-      G[eq.second][eq.second] = 1.0;
-      G[eq.first][eq.second] = val;
-      G[eq.second][eq.first] = 1 / val;
-      V.insert(eq.first);
-      V.insert(eq.second);
+      for (string var : vector<string>{eq.first, eq.second}) {
+        if (V.find(var) == V.end()) {
+          myset[var] = {var, 1.0, 0};
+          V.insert(var);
+        }
+      }
+      _union(myset, eq.first, eq.second, val);
     }
-
-    for (string k : V)
-      for (auto i : G[k])
-        for (auto j : G[k])
-          G[i.first][j.first] = j.second / i.second;
 
     vector<double> ret;
     for (auto& qry : queries) {
       double val = -1;
       if (V.find(qry.first) != V.end() &&
-          V.find(qry.second) != V.end() &&
-          G[qry.first].find(qry.second) != G[qry.first].end())
-        val = G[qry.first][qry.second];
+          V.find(qry.second) != V.end()) {
+        string p0 = _find(myset, qry.first),
+               p1 = _find(myset, qry.second);
+        if (p0 == p1)
+          val = myset[qry.second].val / myset[qry.first].val;
+      }
       ret.push_back(val);
     }
-
     return ret;
+  }
+
+ private:
+  void _union(unordered_map<string, info>& myset,
+              string v0, string v1, double val)
+  {
+    string p0 = _find(myset, v0),
+           p1 = _find(myset, v1);
+    if (p0 == p1) return;
+
+    info &i0 = myset[p0], &i1 = myset[p1];
+    double v01 = myset[v0].val * val / myset[v1].val;
+    if (i0.rank < i1.rank) {
+      i0.par = p1;
+      i0.val = 1 / v01;
+    } else {
+      i1.par = p0;
+      i1.val = v01;
+      i0.rank += i0.rank == i1.rank;
+    }
+  }
+
+  string _find(unordered_map<string, info>& myset, string v0)
+  {
+    info &i = myset[v0];
+    if (i.par != v0) {
+      string oldpar = i.par;
+      i.par = _find(myset, oldpar);
+      i.val *= myset[oldpar].val;
+    }
+    return i.par;
   }
 };
