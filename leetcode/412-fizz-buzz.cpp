8faa7250743e8afa4e714e5150059de8573cb9fb
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-02 19:15:29 -0500

    leetcode 412

---
+++ leetcode/412-fizz-buzz.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+ public:
+  vector<string> fizzBuzz(int n)
+  {
+    vector<string> ret;
+    for (int i = 1; i <= n; ++i) {
+      if (!(i % 3 || i % 5))
+        ret.push_back("FizzBuzz");
+      else if (!(i % 3))
+        ret.push_back("Fizz");
+      else if (!(i % 5))
+        ret.push_back("Buzz");
+      else
+        ret.push_back(to_string(i));
+    }
+    return ret;
+  }
+};
