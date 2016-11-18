bc4af9b601f3e582c097c88f8ed2527296fe45a8
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-18 11:21:55 -0600

    leetcode 358 greedy

---
+++ leetcode/358-rearrange-string-k-distance-apart.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+ public:
+  string rearrangeString(string str, int k)
+  {
+    vector<int> cnt(26, 0), pos(26, -k);
+    for (char c : str) ++cnt[c-'a'];
+
+    string ret;
+    for (int i = 0, maxcnt, maxpos; i < str.size(); ++i) {
+      maxcnt = 1, maxpos = -1;
+      for (int j = 0; j < 26; ++j)
+        if (pos[j] <= i-k && cnt[j] >= maxcnt)
+          maxpos = j, maxcnt = cnt[j];
+      if (maxpos < 0) return "";
+
+      ret += char(maxpos + 'a');
+      --cnt[maxpos];
+      pos[maxpos] = i;
+    }
+    return ret;
+  }
+};
