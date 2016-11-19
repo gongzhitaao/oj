bbd81de9240e0b917a70b2e1668e2cac0dbf9e4a
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-19 12:57:57 -0600

    leetcode 274 bucket sort

--- leetcode/274-h-index.cpp
+++ leetcode/274-h-index.cpp
@@ -3,13 +3,13 @@ class Solution
  public:
   int hIndex(vector<int>& citations)
   {
-    sort(citations.begin(), citations.end());
-    int sz = citations.size();
-    for (int h = sz; h > 0; --h) {
-      int i = sz - h;
-      if (citations[i] >= h && (!i || citations[i-1] <= h))
-        return h;
-    }
+    int n = citations.size();
+    vector<int> bucket(n+1, 0);
+    for (int i : citations)
+      ++bucket[i > n ? n : i];
+    int h = 0;
+    for (int i = n; i > 0; --i)
+      if ((h += bucket[i]) >= i) return i;
     return 0;
   }
 };
