a6eb53cb5caf381de03a2405974d79239ecb57bc
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-19 09:26:23 -0600

    leetcode 409 hashmap

---
+++ leetcode/409-longest-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+ public:
+  int longestPalindrome(string s)
+  {
+    vector<int> cnt(128, 0);
+    for (char c : s) ++cnt[c];
+    int len = 0;
+    for (int i : cnt)
+      len += 0xFFFFFFFE & i;
+    if (len < s.size()) ++len;
+    return len;
+  }
+};
