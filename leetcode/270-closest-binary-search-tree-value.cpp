ace1c45a158b7fee0851c412816b05701802c83f
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-18 21:01:39 -0600

    leetcode 270 clean dfs

--- leetcode/270-closest-binary-search-tree-value.cpp
+++ leetcode/270-closest-binary-search-tree-value.cpp
@@ -12,35 +12,12 @@ class Solution
  public:
   int closestValue(TreeNode* root, double target)
   {
-    for (TreeNode* n = root; ;) {
-      if (n->val > target) {
-        if (!n->left) return n->val;
-        TreeNode* m = rightmost(n->left);
-        if (m->val > target)
-          n = n->left;
-        else return target-m->val < n->val-target ? m->val : n->val;
-      } else {
-        if (!n->right) return n->val;
-        TreeNode* m = leftmost(n->right);
-        if (m->val < target)
-          n = n->right;
-        else return m->val-target < target-n->val ? m->val : n->val;
-      }
+    int ret = root->val;
+    for (TreeNode* n = root; n;
+         n = n->val>target ? n->left : n->right) {
+      if (abs(target-ret) > abs(target-n->val))
+        ret = n->val;
     }
-    return 0;
-  }
-
-  TreeNode* rightmost(TreeNode* node)
-  {
-    if (!node->right)
-      return node;
-    return rightmost(node->right);
-  }
-
-  TreeNode* leftmost(TreeNode* node)
-  {
-    if (!node->left)
-      return node;
-    return leftmost(node->left);
+    return ret;
   }
 };
