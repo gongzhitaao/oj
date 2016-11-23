5f92fbb390b41b6c90f6b09432701f4ea4a3c53b
Zhitao Gong <zhitaao.gong@gmail.com>
2016-11-23 14:46:25 -0600

    leetcode 451 linear sort too slow

--- leetcode/451-sort-characters-by-frequency.cpp
+++ leetcode/451-sort-characters-by-frequency.cpp
@@ -3,12 +3,14 @@ class Solution
  public:
   string frequencySort(string s)
   {
-    int n = numeric_limits<char>::max() + 1;
-    vector<int> cnt(n, 0);
+    int n = s.size();
+    vector<string> sorted(n+1, "");
+    vector<int> cnt(CHAR_MAX, 0);
     for (char c : s) ++cnt[c];
-    sort(s.begin(), s.end(), [&](char a, char b) {
-        return cnt[a] == cnt[b] ? a > b : cnt[a] > cnt[b];
-      });
-    return s;
+    for (int i = 0; i < CHAR_MAX; ++i)
+      sorted[cnt[i]] += string(cnt[i], i);
+    string ret;
+    for (string& e : sorted) ret = e + ret;
+    return ret;
   }
 };
