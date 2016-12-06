ddd2e39285536a71ba0f3e888be3435916e42f02
Zhitao Gong <zhitaao.gong@gmail.com>
2016-12-06 15:24:01 -0600

    leetcode 316 TODO???

--- leetcode/316-remove-duplicate-letters.cpp
+++ leetcode/316-remove-duplicate-letters.cpp
@@ -4,23 +4,19 @@ class Solution
   string removeDuplicateLetters(string s)
   {
     vector<int> cnt(128, 0);
+    vector<bool> v(128, false);
     for (char ch : s) ++cnt[ch];
-    return doit(s, cnt);
-  }
-
-  string doit(string s, vector<int>& cnt)
-  {
-    int minpos = -1, i;
-    for (i = 0; i < s.size(); ++i) {
-      if (!cnt[s[i]]) continue;
-      if (minpos < 0 || s[i] < s[minpos]) minpos = i;
-      if (0 == --cnt[s[i]]) break;
+    string ret = "0";
+    for (char ch : s) {
+      --cnt[ch];
+      if (v[ch]) continue;
+      while (ch < ret.back() && cnt[ret.back()]) {
+        v[ret.back()] = false;
+        ret.pop_back();
+      }
+      ret += ch;
+      v[ch] = true;
     }
-    if (minpos < 0) return "";
-    ++cnt[s[i]];
-    cnt[s[minpos]] = 0;
-    for (int j = minpos+1; j < i; ++j)
-      if (cnt[s[j]]) ++cnt[s[j]];
-    return s[minpos] + doit(s.substr(minpos+1), cnt);
+    return ret.substr(1);
   }
 };
