8b027b2298cae6c9d4f9640c5c68bf2bcd4229f9
Zhitao Gong <zhitaao.gong@gmail.com>
2017-06-08 15:20:45 -0700

    leetcode 482 insert at string back is more efficient

--- leetcode/482-license-key-formatting.cpp
+++ leetcode/482-license-key-formatting.cpp
@@ -1,20 +1,21 @@
 class Solution {
-  public:
-    string licenseKeyFormatting(string S, int K)
-    {
-        string ret;
-        for (int i = S.size()-1, j; i >= 0; ) {
-            for (j = 0; j < K && i >= 0; --i) {
-                if ('-' != S[i]) {
-                    ret = char(toupper(S[i])) + ret;
-                    ++j;
-                }
-            }
-            if (j == K)
-                ret = '-' + ret;
+ public:
+  string licenseKeyFormatting(string S, int K)
+  {
+    string ret;
+    for (int i = S.size()-1, j; i >= 0; ) {
+      for (j = 0; j < K && i >= 0; --i) {
+        if ('-' != S[i]) {
+          ret += char(toupper(S[i]));
+          ++j;
         }
-        if ('-' == ret[0])
-            return ret.substr(1);
-        return ret;
+      }
+      if (j == K)
+        ret += '-';
     }
+    reverse(ret.begin(), ret.end());
+    if ('-' == ret[0])
+      return ret.substr(1);
+    return ret;
+  }
 };
