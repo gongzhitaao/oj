70a728711f4054697c7d429f6723ec31c9789006
Zhitao Gong <zhitaao.gong@gmail.com>
2017-07-04 15:48:19 -0700

    leetcode 139 dp, trie

--- leetcode/139-word-break.cpp
+++ leetcode/139-word-break.cpp
@@ -1,18 +1,43 @@
 class Solution
 {
  public:
-  bool wordBreak(string s, unordered_set<string>& wordDict)
+  bool wordBreak(string s, vector<string>& wordDict)
   {
-    int n = s.size();
-    vector<bool> dp(n+1, false);
+    struct node;
+    typedef shared_ptr<node> nodeptr;
+
+    struct node {
+      bool word;
+      unordered_map<char, nodeptr> next;
+
+      node(bool w = false) : word(w) { }
+    };
+
+    nodeptr head = make_shared<node>();
+    for (string& word : wordDict) {
+      nodeptr n = head;
+      for (int i = word.size() - 1; i >= 0; --i) {
+        char ch = word[i];
+        if (n->next.find(ch) == n->next.end()) {
+          nodeptr p = make_shared<node>();
+          n->next[ch] = p;
+        }
+        n = n->next[ch];
+      }
+      n->word = true;
+    }
+
+    vector<bool> dp(s.size() + 1, false);
     dp[0] = true;
-    for (int i = 1; i <= n; ++i) {
-      for (int j = 0; j < i; ++j) {
-        if (dp[i] = dp[j] &&
-            wordDict.find(s.substr(j, i-j)) != wordDict.end())
+    for (int i = 0; i < s.size(); ++i) {
+      nodeptr n = head;
+      for (int j = i; j >= 0 && n->next.find(s[j]) != n->next.end(); --j) {
+        n = n->next[s[j]];
+        if (n->word && (dp[i+1] = dp[j]))
           break;
       }
     }
-    return dp[n];
+
+    return dp.back();
   }
 };
