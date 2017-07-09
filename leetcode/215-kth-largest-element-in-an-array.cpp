a2c1709fb7e15f5731bb10b3c6cf9f3880d4fc53
Zhitao Gong <zhitaao.gong@gmail.com>
2017-07-09 11:18:05 -0700

    leetcode 215 stl heap

--- leetcode/215-kth-largest-element-in-an-array.cpp
+++ leetcode/215-kth-largest-element-in-an-array.cpp
@@ -3,34 +3,10 @@ class Solution
  public:
   int findKthLargest(vector<int>& nums, int k)
   {
+    make_heap(nums.begin(), nums.end());
     int n = nums.size();
-    for (int i = (n - 2) / 2; i >= 0; --i)
-      max_heapify(nums, i, n);
-
-    int ret = 0;
-    for (int i = 0; ;) {
-      ret = nums[0];
-      if (++i >= k) break;
-
-      swap(nums[0], nums[--n]);
-      max_heapify(nums, 0, n);
-    }
-
-    return ret;
-  }
-
- private:
-  void max_heapify(vector<int>& nums, int i, int n)
-  {
-    int l = 2 * i + 1, r = 2 * i + 2;
-    int k = i;
-
-    if (l < n && nums[l] > nums[k]) k = l;
-    if (r < n && nums[r] > nums[k]) k = r;
-
-    if (k != i) {
-      swap(nums[i], nums[k]);
-      max_heapify(nums, k, n);
-    }
+    for (int i = 0; i < k - 1; ++i, --n)
+      pop_heap(nums.begin(), nums.begin()+n);
+    return nums[0];
   }
 };
