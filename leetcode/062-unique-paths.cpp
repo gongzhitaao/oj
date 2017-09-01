babd9f3a49f355593af504a903f16e64d79febf5
Zhitao Gong <zhitaao.gong@gmail.com>
2017-09-01 15:21:04 -0500

    leetcode 62 simple dp

--- leetcode/062-unique-paths.cpp
+++ leetcode/062-unique-paths.cpp
@@ -1,18 +1,13 @@
-class Solution {
-public:
-  int uniquePaths(int m, int n) {
-    int large, small;
-    if (m > n) large = m, small = n;
-    else large = n, small = m;
-
-    int cnt[large + 1];
-    fill(cnt, cnt + large + 1, 0);
+class Solution
+{
+ public:
+  int uniquePaths(int m, int n)
+  {
+    vector<int> cnt(101, 0);
     cnt[1] = 1;
-
-    for (int i = 1; i <= small; ++i)
-      for (int j = 1; j <= large; ++j)
-        cnt[j] += cnt[j - 1];
-
-    return cnt[large];
+    for (int i = 1; i <= m; ++i)
+      for (int j = 1; j <= n; ++j)
+        cnt[j] += cnt[j-1];
+    return cnt[n];
   }
 };
