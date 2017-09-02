f2a74e73ebd680bb5d1cee46852604145bdf642c
Zhitao Gong <zhitaao.gong@gmail.com>
2017-09-02 10:06:38 -0500

    leetcode 64 dp

--- leetcode/064-minimum-path-sum.cpp
+++ leetcode/064-minimum-path-sum.cpp
@@ -3,34 +3,12 @@ class Solution
  public:
   int minPathSum(vector<vector<int>>& grid)
   {
-    typedef tuple<int, int, int> data_t;
-    struct cmp {
-      bool operator()(const data_t& a, const data_t& b)
-      {
-        return get<0>(a) > get<0>(b);
-      }
-    };
-
     const int row = grid.size(), col = grid[0].size();
-    vector<vector<int>> d(row, vector<int>(col, 0));
-    d[0][0] = grid[0][0];
-    const vector<int> D{0, 1, 0};
-
-    priority_queue<data_t, vector<data_t>, cmp> q;
-    for (q.push({0, 0, 0}); q.size(); ) {
-      data_t cur = q.top(); q.pop();
-      int r = get<1>(cur), c = get<2>(cur);
-      for (int i = 0; i < 2; ++i) {
-        int rr = r + D[i], cc = c + D[i + 1];
-        if (rr < row && cc < col && !d[rr][cc]) {
-          d[rr][cc] = d[r][c] + grid[rr][cc];
-          if (row - 1 == rr && col - 1 == cc)
-            break;
-          q.push({d[rr][cc], rr, cc});
-        }
-      }
-    }
-
-    return d[row - 1][col - 1];
+    vector<int> d(col + 1, numeric_limits<int>::max());
+    d[1] = 0;
+    for (int i = 0; i < row; ++i)
+      for (int j = 0; j < col; ++j)
+        d[j + 1] = min(d[j], d[j + 1]) + grid[i][j];
+    return d[col];
   }
 };
