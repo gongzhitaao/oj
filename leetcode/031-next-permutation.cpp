9ddffe7dfe5f0f020401a4acde67b866ea6c2902
Zhitao Gong <zhitaao.gong@gmail.com>
2017-09-02 14:42:12 -0500

    leetcode 31 next permutation...

--- leetcode/031-next-permutation.cpp
+++ leetcode/031-next-permutation.cpp
@@ -3,6 +3,12 @@ class Solution
  public:
   void nextPermutation(vector<int>& nums)
   {
-    next_permutation(nums.begin(), nums.end());
+    int n = nums.size(), p1, p2;
+    for (p1 = n - 2; p1 >= 0 && nums[p1] >= nums[p1 + 1]; --p1);
+    if (p1 >= 0) {
+      for (p2 = n - 1; p2 > p1 + 1 && nums[p1] >= nums[p2]; --p2);
+      swap(nums[p1], nums[p2]);
+    }
+    reverse(nums.begin() + p1 + 1, nums.end());
   }
 };
