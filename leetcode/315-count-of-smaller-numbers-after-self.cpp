1abbdc193535c6fad68af408df8630da722f22da
Zhitao Gong <zhitaao.gong@gmail.com>
2017-09-07 14:40:38 -0500

    leetcode 315 merge count

--- leetcode/315-count-of-smaller-numbers-after-self.cpp
+++ leetcode/315-count-of-smaller-numbers-after-self.cpp
@@ -3,37 +3,38 @@ class Solution
  public:
   vector<int> countSmaller(vector<int>& nums)
   {
-    vector<int> vec;
-    std::unique_copy(nums.begin(), nums.end(), back_inserter(vec));
-    sort(vec.begin(), vec.end());
+    const int n = nums.size();
+    vector<int> cnt(n, 0), idx(n, 0);
+    for (int i = 0; i < nums.size(); ++i)
+      idx[i] = i;
+    mergesort(nums, cnt, idx, 0, n - 1);
+    return cnt;
+  }
 
-    unordered_map<int, int> pos;
-    for (int i = 0; i < vec.size(); ++i) pos[vec[i]] = i;
+  void mergesort(vector<int>& nums, vector<int>& cnt, vector<int>& idx, int lo,
+                 int hi)
+  {
+    if (lo >= hi) return;
 
-    int n = nums.size();
-    vector<int> ret, bit(n+1, 0);
-    for (int i = n-1; i >= 0; --i) {
-      int p = pos[nums[i]];
-      update(bit, p+1, 1);
-      ret.push_back(getsum(bit, p));
+    int mi = lo + (hi - lo) / 2;
+    mergesort(nums, cnt, idx, lo, mi);
+    mergesort(nums, cnt, idx, mi + 1, hi);
+
+    vector<int> work(hi - lo + 1, 0), idx1(hi - lo + 1, 0);
+
+    int i, j, k, c;
+    for (c = 0, i = lo, j = mi + 1, k = 0; i <= mi && j <= hi; ++k) {
+      if (nums[i] <= nums[j])
+        work[k] = nums[i], idx1[k] = idx[i], ++i, cnt[idx1[k]] += c;
+      else
+        work[k] = nums[j], idx1[k] = idx[j], ++j, ++c;
     }
+    for (; i <= mi; ++k, ++i)
+      work[k] = nums[i], idx1[k] = idx[i], cnt[idx1[k]] += c;
+    for (; j <= hi; ++k, ++j)
+      work[k] = nums[j], idx1[k] = idx[j];
 
-    reverse(ret.begin(), ret.end());
-    return ret;
-  }
-
-  int getsum(vector<int>& bit, int i)
-  {
-    int s = 0;
-    for (; i > 0; i -= i & -i)
-      s += bit[i];
-    return s;
-  }
-
-  void update(vector<int>& bit, int i, int v)
-  {
-    int n = bit.size();
-    for (; i < n; i += i & -i)
-      bit[i] += v;
+    copy(work.begin(), work.end(), nums.begin() + lo);
+    copy(idx1.begin(), idx1.end(), idx.begin() + lo);
   }
 };
