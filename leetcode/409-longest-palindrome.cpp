eefb3478812ade0a0c37ca09822507e7afd8cbfd
Zhitao Gong <zhitaao.gong@gmail.com>
2017-09-08 14:20:03 -0500

    leetcode 409 code format

--- leetcode/409-longest-palindrome.cpp
+++ leetcode/409-longest-palindrome.cpp
@@ -6,8 +6,7 @@ class Solution
     vector<int> cnt(128, 0);
     for (char c : s) ++cnt[c];
     int len = 0;
-    for (int i : cnt)
-      len += 0xFFFFFFFE & i;
+    for (int i : cnt) len += 0xFFFFFFFE & i;
     if (len < s.size()) ++len;
     return len;
   }
