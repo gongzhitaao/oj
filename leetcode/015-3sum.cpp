69e1d2d5178876be24473fe0e87bbb9a36fddfd4
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-02 20:17:31 -0500

    leetcode 15 3sum fb

--- leetcode/015-3sum.cpp
+++ leetcode/015-3sum.cpp
@@ -3,22 +3,35 @@ class Solution
  public:
   vector<vector<int>> threeSum(vector<int>& nums)
   {
-    vector<vector<int>> ret;
     sort(nums.begin(), nums.end());
+
     const int n = nums.size();
-    for (int i = 0; i < n - 2; ++i) {
+    vector<vector<int>> ret;
+
+    for (int i = 0; i < n; ++i) {
       if (i > 0 && nums[i] == nums[i - 1]) continue;
-      int sum = -nums[i];
-      for (int j = i + 1, k = n - 1, b, c; j < k;) {
-        if (nums[j] + nums[k] < sum) ++j;
-        else if (nums[j] + nums[k] > sum) --k;
-        else {
-          ret.push_back({nums[i], nums[j], nums[k]});
-          for (++j; j > i + 1 && j < k && nums[j] == nums[j - 1]; ++j);
-          for (--k; k < n - 1 && k > j && nums[k] == nums[k + 1]; --k);
+
+      int sum = nums[i];
+      for (int j = i + 1, k = n - 1; j < k;) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) {
+          ++j;
+          continue;
         }
+
+        if (k < n - 1 && nums[k] == nums[k + 1]) {
+          --k;
+          continue;
+        }
+
+        if (nums[j] + nums[k] + sum > 0)
+          --k;
+        else if (nums[j] + nums[k] + sum < 0)
+          ++j;
+        else
+          ret.push_back({nums[i], nums[j++], nums[k--]});
       }
     }
+
     return ret;
   }
 };
