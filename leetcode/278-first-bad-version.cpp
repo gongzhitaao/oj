4d523b14acd99d7c95b635eb466a372256acf0df
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-03 19:56:24 -0500

    leetcode 278 binary search
    Note that exit condition

--- leetcode/278-first-bad-version.cpp
+++ leetcode/278-first-bad-version.cpp
@@ -1,16 +1,18 @@
 // Forward declaration of isBadVersion API.
 bool isBadVersion(int version);
 
-class Solution {
-public:
-  int firstBadVersion(int n) {
-    int ret = -1;
-    for (int l = 1, h = n; l <= h; ) {
-      int m = ((long long)l + h) / 2;
-      if (isBadVersion(m)) ret = m, h = m - 1;
-      else l = m + 1;
+class Solution
+{
+ public:
+  int firstBadVersion(int n)
+  {
+    int lo = 1, hi = n, mi;
+    for (; lo < hi; ) {
+      mi = lo + (hi - lo) / 2;
+      if (!isBadVersion(mi))
+        lo = mi + 1;
+      else hi = mi;
     }
-
-    return ret;
+    return lo;
   }
 };
