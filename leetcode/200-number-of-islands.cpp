46b98baca54e60978b8adce5344982f2e177b918
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-04 16:51:13 -0500

    leetcode 200 bfs/dfs

--- leetcode/200-number-of-islands.cpp
+++ leetcode/200-number-of-islands.cpp
@@ -1,33 +1,35 @@
 class Solution
 {
  public:
-  int numIslands(vector<vector<char> >& grid)
+  int numIslands(vector<vector<char>>& grid)
   {
     if (grid.empty() || grid[0].empty()) return 0;
 
-    int nrow = grid.size(), ncol = grid[0].size();
-    const vector<int> step{-1, 0, 1, 0, -1};
-
-    typedef pair<int, int> id;
+    const int row = grid.size(), col = grid[0].size();
+    int cur = 0, total = row * col;
     int cnt = 0;
-    queue<id> q;
-    for (int i = 0; i < nrow; ++i) {
-      for (int j = 0; j < ncol; ++j) {
-        if ('0' == grid[i][j]) continue;
-        ++cnt;
-        for (q.push({i, j}); !q.empty(); ) {
-          id p = q.front(); q.pop();
-          for (int k = 0; k < 4; ++k) {
-            int r = p.first + step[k], c = p.second + step[k+1];
-            if (0 <= r && r < nrow && 0 <= c && c < ncol &&
-                '1' == grid[r][c]) {
-              grid[r][c] = '0';
-              q.push({r, c});
-            }
-          }
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+        if ('1' == grid[i][j]) {
+          ++cnt;
+          cur += dfs(grid, i, j);
+          if (cur == total) return cnt;
         }
       }
     }
+
     return cnt;
+  }
+
+  int dfs(vector<vector<char>>& grid, int i, int j)
+  {
+    grid[i][j] = 'x';
+    int n = 1;
+    if (i > 0 && '1' == grid[i - 1][j]) n += dfs(grid, i - 1, j);
+    if (j + 1 < grid[0].size() && '1' == grid[i][j + 1])
+      n += dfs(grid, i, j + 1);
+    if (i + 1 < grid.size() && '1' == grid[i + 1][j]) n += dfs(grid, i + 1, j);
+    if (j > 0 && '1' == grid[i][j - 1]) n += dfs(grid, i, j - 1);
+    return n;
   }
 };
