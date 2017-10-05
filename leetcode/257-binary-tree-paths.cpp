89ab2e258038af0498e59fa2cf3984c045b6b6e5
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-05 16:14:30 -0500

    leetcode 257 dfs

--- leetcode/257-binary-tree-paths.cpp
+++ leetcode/257-binary-tree-paths.cpp
@@ -7,31 +7,21 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-public:
-  vector<string> binaryTreePaths(TreeNode* root) {
-    vector<string> vec;
-    if (!root) return vec;
-
-    string c = to_string(root->val);
-    if (!root->left && !root->right) {
-      vec.push_back(c);
-      return vec;
-    }
-
-    dfs(root->left, c, vec);
-    dfs(root->right, c, vec);
-    return vec;
+class Solution
+{
+ public:
+  vector<string> binaryTreePaths(TreeNode* root)
+  {
+    vector<string> ret;
+    if (root) dfs(root, ret, "");
+    return ret;
   }
 
-  void dfs(const TreeNode *root, const string &p, vector<string> &vec) {
-    if (!root) return;
-
-    string c = to_string(root->val);
-    if (!root->left && !root->right)
-      vec.push_back(p + "->" + c);
-
-    dfs(root->left, p + "->" + c, vec);
-    dfs(root->right, p + "->" + c, vec);
+  void dfs(TreeNode* cur, vector<string>& ret, string path)
+  {
+    path += "->" + to_string(cur->val);
+    if (!cur->left && !cur->right) return ret.push_back(path.substr(2));
+    if (cur->left) dfs(cur->left, ret, path);
+    if (cur->right) dfs(cur->right, ret, path);
   }
 };
