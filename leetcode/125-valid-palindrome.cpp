4a06990b80a3a027fb3ed30c9f31a0f94b1958d5
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-06 16:23:02 -0500

    leetcode 125 ad hoc

--- leetcode/125-valid-palindrome.cpp
+++ leetcode/125-valid-palindrome.cpp
@@ -1,18 +1,16 @@
-class Solution {
-public:
-  bool isPalindrome(string s) {
-    for (int i = 0, j = s.size() - 1; i < j; ) {
-      char a = tolower(s[i]), b = tolower(s[j]);
-
-      if (isalnum(a) && isalnum(b)) {
-        if (a != b) return false;
-        ++i, --j;
-      } else {
-        if (!isalnum(a)) ++i;
-        if (!isalnum(b)) --j;
-      }
+class Solution
+{
+ public:
+  bool isPalindrome(string s)
+  {
+    for (int i = 0, j = s.size() - 1; i < j;) {
+      if (!isalnum(s[i]))
+        ++i;
+      else if (!isalnum(s[j]))
+        --j;
+      else if (tolower(s[i++]) != tolower(s[j--]))
+        return false;
     }
-
     return true;
   }
 };
