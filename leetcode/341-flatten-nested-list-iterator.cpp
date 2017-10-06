832dda15d42d2d2ac58d0dadbfa9f15df477485d
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-06 15:20:48 -0500

    leetcode 341 dfs

--- leetcode/341-flatten-nested-list-iterator.cpp
+++ leetcode/341-flatten-nested-list-iterator.cpp
@@ -3,47 +3,51 @@
  * // You should not implement it, or speculate about its implementation
  * class NestedInteger {
  *   public:
- *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     // Return true if this NestedInteger holds a single integer, rather than
+ * a nested list.
  *     bool isInteger() const;
  *
- *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return the single integer that this NestedInteger holds, if it holds a
+ * single integer
  *     // The result is undefined if this NestedInteger holds a nested list
  *     int getInteger() const;
  *
- *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return the nested list that this NestedInteger holds, if it holds a
+ * nested list
  *     // The result is undefined if this NestedInteger holds a single integer
  *     const vector<NestedInteger> &getList() const;
  * };
  */
 class NestedIterator
 {
+  vector<int> flat_;
+  int ind_;
+
+  void dfs(const vector<NestedInteger>& vni)
+  {
+    for (const NestedInteger& elm : vni) {
+      if (elm.isInteger())
+        flat_.push_back(elm.getInteger());
+      else dfs(elm.getList());
+    }
+  }
+
  public:
   NestedIterator(vector<NestedInteger> &nestedList)
-      : ptr_(0)
   {
-    visit(nestedList);
+    ind_ = 0;
+    dfs(nestedList);
   }
 
   int next()
   {
-    return flat_[ptr_++];
+    return flat_[ind_++];
   }
 
   bool hasNext()
   {
-    return ptr_ < flat_.size();
+    return ind_ < flat_.size();
   }
-
- private:
-  void visit(vector<NestedInteger> &vec)
-  {
-    for (auto& i : vec)
-      if (i.isInteger()) flat_.push_back(i.getInteger());
-      else visit(i.getList());
-  }
-
-  vector<int> flat_;
-  int ptr_;
 };
 
 /**
