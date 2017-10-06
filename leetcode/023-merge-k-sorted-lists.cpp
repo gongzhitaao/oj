8a39d9b388afbaae86c4ecd9c43837ac568219be
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-06 09:22:05 -0500

    leetcode 23 priority queue fb

--- leetcode/023-merge-k-sorted-lists.cpp
+++ leetcode/023-merge-k-sorted-lists.cpp
@@ -6,36 +6,35 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
-class Solution {
+class Solution
+{
  public:
   ListNode* mergeKLists(vector<ListNode*>& lists)
   {
     if (lists.empty()) return nullptr;
-    return merge(lists, 0, lists.size());
-  }
 
-  ListNode* merge(vector<ListNode*>& lists, int start, int len)
-  {
-    if (1 == len) return lists[start];
-    int half = len / 2;
-    ListNode* left = merge(lists, start, half);
-    ListNode* right = merge(lists, start+half, len-half);
-    ListNode* head = new ListNode(0);
-    ListNode* cur;
-    for (cur = head; left && right; cur = cur->next) {
-      if (left->val < right->val) {
-        cur->next = left;
-        left = left->next;
-      } else {
-        cur->next = right;
-        right = right->next;
-      }
+    struct cmp {
+      bool operator()(ListNode* a, ListNode* b) { return a->val > b->val; }
+    };
+
+    priority_queue<ListNode*, vector<ListNode*>, cmp> q;
+
+    for (ListNode* elm : lists)
+      if (elm) q.push(elm);
+
+    if (q.empty()) return nullptr;
+
+    ListNode* head = q.top();
+    q.pop();
+    if (head->next) q.push(head->next);
+
+    for (ListNode *cur = head, *ptr = nullptr; q.size(); cur = cur->next) {
+      ptr = q.top();
+      q.pop();
+      cur->next = ptr;
+      if (ptr->next) q.push(ptr->next);
     }
-    if (left) cur->next = left;
-    else if (right) cur->next = right;
-    cur = head->next;
-    delete head;
-    return cur;
+
+    return head;
   }
 };
