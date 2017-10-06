0017018eabfd5772b3be7e71395c551862e41f13
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-06 09:27:24 -0500

    leetcode 157 ad hoc
    We could not put chars back to stream, but we could save it !!!

---
+++ leetcode/157-read-n-characters-given-read4.cpp
@@ -0,0 +1,25 @@
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution
+{
+  char buf_[4];
+  char i_;
+  char n_;
+
+ public:
+  Solution() : i_(0), n_(0) {}
+  /**
+   * @param buf Destination buffer
+   * @param n   Maximum number of characters to read
+   * @return    The number of characters read
+   */
+  int read(char *buf, int n)
+  {
+    int cnt;
+    for (cnt = 0; cnt < n && ((i_ < n_) || ((i_ = 0) < (n_ = read4(buf_))));
+         ++cnt, ++i_)
+      buf[cnt] = buf_[i_];
+    return cnt;
+  }
+};
