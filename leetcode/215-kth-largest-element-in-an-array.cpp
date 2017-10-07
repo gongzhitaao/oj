b0c7b0e9c8aeb8108e1e98fa5f812a30dafb28e3
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-07 17:27:46 -0500

    leetcode 215 binary search

--- leetcode/215-kth-largest-element-in-an-array.cpp
+++ leetcode/215-kth-largest-element-in-an-array.cpp
@@ -3,10 +3,22 @@ class Solution
  public:
   int findKthLargest(vector<int>& nums, int k)
   {
-    make_heap(nums.begin(), nums.end());
-    int n = nums.size();
-    for (int i = 0; i < k - 1; ++i, --n)
-      pop_heap(nums.begin(), nums.begin()+n);
-    return nums[0];
+    int lo, hi, mi;
+    for (lo = 0, hi = nums.size() - 1; lo < hi; ) {
+      mi = lo + (hi - lo) / 2;
+      swap(nums[lo], nums[mi]);
+
+      int p = lo;
+      for (int j = lo + 1; j <= hi; ++j)
+        if (nums[j] > nums[lo]) swap(nums[++p], nums[j]);
+      swap(nums[lo], nums[p]);
+
+      if (p + 1 == k) return nums[p];
+
+      if (p + 1 < k) lo = p + 1;
+      else hi = p - 1;
+    }
+
+    return nums[lo];
   }
 };
