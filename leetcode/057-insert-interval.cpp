377b8d85f8e4366a50330fbb703ee64f2048d0e5
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-07 15:05:05 -0500

    leetcode 57 binary search

--- leetcode/057-insert-interval.cpp
+++ leetcode/057-insert-interval.cpp
@@ -7,32 +7,56 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
-class Solution {
+class Solution
+{
  public:
-  vector<Interval> insert(vector<Interval>& intervals,
-                          Interval newInterval)
+  vector<Interval> insert(vector<Interval>& intervals, Interval itv)
   {
+    if (intervals.empty()) return {itv};
+
     vector<Interval> ret;
-    Interval newitv = newInterval;
-    bool inserted = false;
-    for (Interval& itv : intervals) {
-      if (inserted) ret.push_back(itv);
-      else {
-        if (newitv.end < itv.start) {
-          inserted = true;
-          ret.push_back(newitv);
-          ret.push_back(itv);
-        } else if (newitv.start > itv.end) {
-          ret.push_back(itv);
-        } else {
-          newitv.start = min(newitv.start, itv.start);
-          newitv.end = max(newitv.end, itv.end);
-        }
-      }
+
+    if (itv.end < intervals[0].start) {
+      ret.push_back(itv);
+      copy(intervals.begin(), intervals.end(), back_inserter(ret));
+      return ret;
     }
 
-    if (!inserted)
-      ret.push_back(newitv);
+    if (itv.start > intervals.back().end) {
+      ret = intervals;
+      ret.push_back(itv);
+      return ret;
+    }
+
+    int lo, mi, hi;
+
+    for (lo = 0, hi = intervals.size() - 1; lo < hi; ) {
+      mi = lo + (hi - lo) / 2;
+      Interval& cur = intervals[mi];
+      if (cur.end < itv.start) lo = mi + 1;
+      else hi = mi;
+    }
+
+    int p1 = lo;
+
+    for (lo = 0, hi = intervals.size() - 1; lo < hi; ) {
+      mi = lo + (hi - lo + 1) / 2;
+      Interval& cur = intervals[mi];
+      if (cur.start > itv.end) hi = mi - 1;
+      else lo = mi;
+    }
+
+    int p2 = lo;
+
+    for (int i = 0; i < p1; ++i)
+      ret.push_back(intervals[i]);
+
+    itv.start = min(itv.start, intervals[p1].start);
+    itv.end = max(itv.end, intervals[p2].end);
+    ret.push_back(itv);
+
+    for (int i = p2 + 1; i < intervals.size(); ++i)
+      ret.push_back(intervals[i]);
 
     return ret;
   }
