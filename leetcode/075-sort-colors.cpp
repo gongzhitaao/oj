721356a1d625efdf08aab9c28c609609048596fe
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-07 14:09:15 -0500

    leetcode 75 two pointers

--- leetcode/075-sort-colors.cpp
+++ leetcode/075-sort-colors.cpp
@@ -1,17 +1,12 @@
-class Solution {
-public:
-  void sortColors(vector<int>& nums) {
-    for (int i = 0, j = nums.size() - 1, k = 0; k <= j; ) {
-      if (0 == nums[k]) {
-        swap(nums[i], nums[k]);
-        ++i;
-        ++k;
-      } else if (2 == nums[k]) {
-        swap(nums[j], nums[k]);
-        --j;
-      } else {
-        ++k;
-      }
+class Solution
+{
+ public:
+  void sortColors(vector<int>& nums)
+  {
+    for (int i = -1, j = nums.size(), k = 0; k < j; ) {
+      if (0 == nums[k]) swap(nums[k++], nums[++i]);
+      else if (1 == nums[k]) ++k;
+      else swap(nums[k], nums[--j]);
     }
   }
 };
