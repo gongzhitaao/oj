c7ecd2ad1967fd7050b01315b67732de5fb73173
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-07 13:58:31 -0500

    leetcode 43 ad hoc

--- leetcode/043-multiply-strings.cpp
+++ leetcode/043-multiply-strings.cpp
@@ -3,30 +3,33 @@ class Solution
  public:
   string multiply(string num1, string num2)
   {
-    if (num1.size() > num2.size())
-      return multiply(num2, num1);
+    if (num1.size() < num2.size()) swap(num1, num2);
 
-    if ("0" == num1 || "0" == num2)
-      return "0";
+    if ("0" == num2 || "0" == num1) return "0";
+    if ("1" == num2) return num1;
 
-    int n1 = num1.size(), n2 = num2.size();
-    string ret(n1+n2, '0');
-
-    for (int i = n1-1, k; i >= 0; --i) {
-      int carry = 0, a = num1[i]-'0';
-      k = n2 + i;
-      for (int j = n2-1; j >= 0; --j, --k) {
-        int tmp = a * (num2[j]-'0') + ret[k]-'0' + carry;
-        carry = tmp / 10;
-        ret[k] = (tmp % 10) + '0';
+    string res(num1.size() + num2.size() + 1, '0');
+    for (int i = num2.size() - 1, j, k; i >= 0; --i) {
+      int carry = 0;
+      for (j = num1.size() - 1, k = num2.size() - 1 - i; j >= 0; --j, ++k) {
+        int out = carry + (num2[i] - '0') * (num1[j] - '0') + res[k] - '0';
+        res[k] = out % 10 + '0';
+        carry = out / 10;
       }
-      ret[k] += carry;
-      cout << ret << endl;
+
+      for (; carry; ++k) {
+        int out = carry + res[k] - '0';
+        res[k] = out % 10 + '0';
+        carry = out / 10;
+      }
     }
 
-    int pos = 0;
-    for (; '0' == ret[pos]; ++pos) /* empty */;
+    int p = res.size() - 1;
+    for (; p >= 0 && '0' == res[p]; --p)
+      ;
+    res = res.substr(0, p + 1);
+    reverse(res.begin(), res.end());
 
-    return ret.substr(pos);
+    return res;
   }
 };
