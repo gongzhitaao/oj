71894a3565ca83e0315f621e8be1876fd0787670
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-07 12:36:57 -0500

    leetcode 78 incremental

--- leetcode/078-subsets.cpp
+++ leetcode/078-subsets.cpp
@@ -1,13 +1,14 @@
-class Solution {
+class Solution
+{
  public:
-  vector<vector<int> > subsets(vector<int>& nums)
+  vector<vector<int>> subsets(vector<int>& nums)
   {
-    vector<vector<int> > ret = {{}};
+    vector<vector<int>> ret = {{}};
     for (int i = 0; i < nums.size(); ++i) {
       for (int j = 0, n = ret.size(); j < n; ++j) {
-        vector<int> vec = ret[j];
-        vec.push_back(nums[i]);
-        ret.push_back(vec);
+        vector<int> v = ret[j];
+        v.push_back(nums[i]);
+        ret.push_back(v);
       }
     }
     return ret;
