0990b5affca2cfa57c15908a4b6fe6c8a4221e8e
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-07 12:03:02 -0500

    leetcode 285 binary search

--- leetcode/285-inorder-successor-in-bst.cpp
+++ leetcode/285-inorder-successor-in-bst.cpp
@@ -12,24 +12,15 @@ class Solution
  public:
   TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p)
   {
-    TreeNode* tmp;
-    if (root)
-      return inorder(root, tmp, p);
-    return nullptr;
-  }
-
-  TreeNode* inorder(TreeNode* cur, TreeNode*& par, TreeNode* p)
-  {
-    TreeNode* ret = nullptr;
-
-    if (cur->left) ret = inorder(cur->left, par, p);
-    if (ret) return ret;
-
-    if (par == p) return cur;
-    par = cur;
-
-    if (cur->right) ret = inorder(cur->right, par, p);
-
-    return ret;
+    TreeNode* cand = nullptr;
+    for (TreeNode* cur = root; cur;) {
+      if (cur->val <= p->val)
+        cur = cur->right;
+      else {
+        cand = cur;
+        cur = cur->left;
+      }
+    }
+    return cand;
   }
 };
