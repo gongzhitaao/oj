1b5482a4b041219a64c6cbd9cceefc417d4ace51
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-08 14:54:57 -0500

    leetcode 38 simulation

--- leetcode/038-count-and-say.cpp
+++ leetcode/038-count-and-say.cpp
@@ -1,22 +1,22 @@
-class Solution {
-public:
-  string countAndSay(int n) {
-    string ret = "1";
-    for (int i = 0; i < n - 1; ++i) {
-      string t = "";
-      char c = ret[0];
-      int cnt = 1;
-      for (int j = 1; j < ret.size(); ++j) {
-        if (ret[j] == c) ++cnt;
+class Solution
+{
+ public:
+  string countAndSay(int n)
+  {
+    string ret = "1#";
+    for (int i = 0, j = 0, k = 0; i < n - 1; ++i) {
+      string tmp;
+      for (j = 1, k = 1; j < ret.size(); ++j) {
+        if (ret[j] == ret[j - 1])
+          ++k;
         else {
-          t += to_string(cnt) + c;
-          c = ret[j];
-          cnt = 1;
+          tmp += to_string(10 * k + ret[j - 1] - '0');
+          k = 1;
         }
       }
-      ret = t + to_string(cnt) + c;
+      ret = tmp + "#";
     }
-
+    ret.pop_back();
     return ret;
   }
 };
