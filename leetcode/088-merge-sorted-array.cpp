100f7d42709a4912e82bd207429ee900595e62f4
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-08 15:37:10 -0500

    leetcode 88 merge

--- leetcode/088-merge-sorted-array.cpp
+++ leetcode/088-merge-sorted-array.cpp
@@ -1,11 +1,14 @@
-class Solution {
-public:
-  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    for (int i = m + n - 1, j1 = m - 1, j2 = n - 1; i >= 0; --i) {
-      if (j2 < 0) nums1[i] = nums1[j1--];
-      else if (j1 < 0) nums1[i] = nums2[j2--];
-      else if (nums1[j1] > nums2[j2]) nums1[i] = nums1[j1--];
-      else nums1[i] = nums2[j2--];
+class Solution
+{
+ public:
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+  {
+    int i, j, k;
+    for (k = m + n - 1, i = m - 1, j = n - 1; k >= 0 && i >= 0 && j >= 0; --k) {
+      if (nums1[i] > nums2[j]) nums1[k] = nums1[i--];
+      else nums1[k] = nums2[j--];
     }
+    for (; j >= 0; --k, --j)
+      nums1[k] = nums2[j];
   }
 };
