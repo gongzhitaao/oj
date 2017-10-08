b00dd92fb73f140e2d2b74e6de593e920d5ea8ad
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-08 14:42:00 -0500

    leetcode 477 hamming distance

--- leetcode/477-total-hamming-distance.cpp
+++ leetcode/477-total-hamming-distance.cpp
@@ -3,15 +3,13 @@ class Solution
  public:
   int totalHammingDistance(vector<int>& nums)
   {
-    vector<int> cnt(32, 0);
-    for (int n : nums) {
-      for (int i = 0; n; ++i, n >>= 1)
-        cnt[i] += (n & 1);
+    int d = 0;
+    for (int i = 1; i < 1000000000; i <<= 1) {
+      int k = 0;
+      for (int n : nums)
+        if (n & i) ++k;
+      d += k * (nums.size() - k);
     }
-    int sum = 0;
-    int n = nums.size();
-    for (int k : cnt)
-      sum += k * (n - k);
-    return sum;
+    return d;
   }
 };
