6712487d577a2efea3e0f7c1e5c8f00069fb36a6
Zhitao Gong <zhitaao.gong@gmail.com>
2017-10-10 16:04:59 -0500

    leetcode 3 substring

--- leetcode/003-longest-substring-without-repeating-characters.cpp
+++ leetcode/003-longest-substring-without-repeating-characters.cpp
@@ -3,13 +3,12 @@ class Solution
  public:
   int lengthOfLongestSubstring(string s)
   {
-    vector<int> cnt(128, 0);
+    vector<int> pos(128, -1);
     int maxlen = 0;
-    for (int i = 0, j = 0; j < s.size(); ++j) {
-      ++cnt[s[j]];
-      for (; i < j && cnt[s[j]] > 1; --cnt[s[i++]])
-        ;
-      if (j - i + 1 > maxlen) maxlen = j - i + 1;
+    for (int i = 0, start = -1; i < s.size(); ++i) {
+      if (pos[s[i]] > start) start = pos[s[i]];
+      if (i - start > maxlen) maxlen = i - start;
+      pos[s[i]] = i;
     }
     return maxlen;
   }
