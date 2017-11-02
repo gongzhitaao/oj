3005602df90af9f30e621a911ff04771ec519c7e
Zhitao Gong <zhitaao.gong@gmail.com>
2017-11-01 21:05:21 -0500

    leetcode 26

--- leetcode/026-remove-duplicates-from-sorted-array.cpp
+++ leetcode/026-remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,15 @@
-class Solution {
-public:
-  int removeDuplicates(vector<int>& nums) {
-    if (nums.empty()) return 0;
-
-    int len = 1, pos = 1, pre = nums[0];
+class Solution
+{
+ public:
+  int removeDuplicates(vector<int>& nums)
+  {
+    int len = 1;
     for (int i = 1; i < nums.size(); ++i) {
-      if (nums[i] != pre) {
-        pre = nums[i];
-        nums[pos] = pre;
-        ++pos;
+      if (nums[i] != nums[i - 1]) {
+        if (i > len) nums[len] = nums[i];
         ++len;
       }
     }
-
-    return len;
+    return min(len, (int)nums.size());
   }
 };
