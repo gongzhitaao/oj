90a74274cdb68edb7e76a0918b9490d697ed2a00
Zhitao Gong <zhitaao.gong@gmail.com>
2017-11-07 10:15:53 -0600

    leetcode 48 transpose + flip around y-axis

--- leetcode/048-rotate-image.cpp
+++ leetcode/048-rotate-image.cpp
@@ -4,15 +4,11 @@ class Solution
   void rotate(vector<vector<int>>& matrix)
   {
     const int n = matrix.size();
-    for (int i = 0; i < n / 2; ++i) {
-      int k = n - i - 1;
-      for (int j = i; j < k; ++j) {
-        int tmp = matrix[i][j];
-        matrix[i][j] = matrix[n - 1 - j][i];
-        matrix[n - 1 - j][i] = matrix[k][n - 1 - j];
-        matrix[k][n - 1 - j] = matrix[j][k];
-        matrix[j][k] = tmp;
-      }
-    }
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < i; ++j)
+        swap(matrix[i][j], matrix[j][i]);
+
+    for (int i = 0; i < n; ++i)
+      reverse(matrix[i].begin(), matrix[i].end());
   }
 };
