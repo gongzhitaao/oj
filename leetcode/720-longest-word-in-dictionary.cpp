5550eded9185657201cbe00bf678069d8651511d
Zhitao Gong <zhitaao.gong@gmail.com>
2017-11-10 10:33:09 -0600

    leetcode 720 dfs + trie

--- leetcode/720-longest-word-in-dictionary.cpp
+++ leetcode/720-longest-word-in-dictionary.cpp
@@ -1,33 +1,41 @@
 class Solution
 {
  public:
-  string longestWord(vector<string> &words)
+  struct Node;
+  typedef shared_ptr<Node> nodeptr;
+
+  struct Node {
+    bool word;
+    map<char, nodeptr> next;
+    Node() : word(false) {}
+  };
+
+  string longestWord(vector<string>& words)
   {
-    struct Node;
-    typedef shared_ptr<Node> nodeptr;
-
-    struct Node {
-      bool word;
-      unordered_map<char, nodeptr> next;
-      Node() : word(false) {}
-    };
-
-    sort(words.begin(), words.end());
-
     nodeptr dict = make_shared<Node>();
-    string ret;
     for (string& word : words) {
-      int len = 1;
       nodeptr cur = dict;
       for (char ch : word) {
         if (cur->next.find(ch) == cur->next.end())
           cur->next[ch] = make_shared<Node>();
         cur = cur->next[ch];
-        len += cur->word;
       }
       cur->word = true;
-      if (len == word.size() && len > ret.size())
-        ret = word;
+    }
+
+    return dfs(dict, "");
+  }
+
+  string dfs(nodeptr& cur, string word)
+  {
+    string ret;
+    if (cur->word) ret = word;
+
+    for (auto& child : cur->next) {
+      if (child.second->word) {
+        string tmp = dfs(child.second, word + child.first);
+        if (tmp.size() > ret.size()) ret = tmp;
+      }
     }
 
     return ret;
