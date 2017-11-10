62dd92839dbb579e40130fc494fd24f8a2dff1a0
Zhitao Gong <zhitaao.gong@gmail.com>
2017-11-10 09:10:41 -0600

    leetcode 268 re-format code

--- leetcode/268-missing-number.cpp
+++ leetcode/268-missing-number.cpp
@@ -1,6 +1,8 @@
-class Solution {
-public:
-  int missingNumber(vector<int>& nums) {
+class Solution
+{
+ public:
+  int missingNumber(vector<int>& nums)
+  {
     int sum = 0;
     for (int i : nums) sum += i;
     return (1 + nums.size()) * nums.size() / 2 - sum;
