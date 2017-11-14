da49cc4f85631ad52e1d58de6ae15b04840efccb
Zhitao Gong <zhitaao.gong@gmail.com>
2017-11-13 20:05:39 -0600

    leetcode 188 dp, corner case when k >= n/2

--- leetcode/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ leetcode/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,8 +4,15 @@ class Solution
   int maxProfit(int k, vector<int>& prices)
   {
     const int n = prices.size();
+
+    if (k >= n / 2) {
+      int p = 0;
+      for (int i = 0; i < n - 1; ++i)
+        if (prices[i + 1] > prices[i]) p += prices[i + 1] - prices[i];
+      return p;
+    }
+
     vector<long long> sell(n + 1, 0), tmp(n + 1, 0);
-    if (k > n) k = n;
     for (int i = 1; i <= k; ++i) {
       long long hold = numeric_limits<int>::min();;
       swap(sell, tmp);
