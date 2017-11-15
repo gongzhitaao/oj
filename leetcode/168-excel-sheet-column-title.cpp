d71bf40f9e5b3d412e663648bcb648de9470514b
Zhitao Gong <zhitaao.gong@gmail.com>
2017-11-15 09:33:15 -0600

    leetcode 168

--- leetcode/168-excel-sheet-column-title.cpp
+++ leetcode/168-excel-sheet-column-title.cpp
@@ -1,12 +1,12 @@
-class Solution {
-public:
-  string convertToTitle(int n) {
-    string ret = "";
-    for (int i = n; i ; i = (i - 1) / 26) {
-      int j = (i - 1) % 26;
-      ret = (char)(j + 'A') + ret;
-    }
-
+class Solution
+{
+ public:
+  string convertToTitle(int n)
+  {
+    string ret;
+    for (int i = n; i; i = (i - 1) / 26)
+      ret += ((i - 1) % 26) + 'A';
+    reverse(ret.begin(), ret.end());
     return ret;
   }
 };
