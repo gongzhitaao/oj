00ec60c3c94431e1bceb850246f5423015bccbfd
Zhitao Gong <zhitaao.gong@gmail.com>
2017-12-26 08:37:44 -0600

    leetcode 145 morris postorder

--- leetcode/145-binary-tree-postorder-traversal.cpp
+++ leetcode/145-binary-tree-postorder-traversal.cpp
@@ -13,30 +13,24 @@ class Solution
   vector<int> postorderTraversal(TreeNode* root)
   {
     vector<int> ret;
-    if (!root) return ret;
-
-    stack<TreeNode*> s;
-    for (TreeNode* cur = root; true;) {
-      for (; cur; cur = cur->left) {
-        if (cur->right) s.push(cur->right);
-        s.push(cur);
-      }
-
-      cur = s.top();
-      s.pop();
-
-      if (cur->right && s.size() && s.top() == cur->right) {
-        s.pop();
-        s.push(cur);
-        cur = cur->right;
+    for (TreeNode *cur = root, *tmp = nullptr; cur;) {
+      if (cur->right) {
+        for (tmp = cur->right; tmp->left && tmp->left != cur; tmp = tmp->left)
+          ;
+        if (tmp->left) {
+          tmp->left = nullptr;
+          cur = cur->left;
+        } else {
+          tmp->left = cur;
+          ret.push_back(cur->val);
+          cur = cur->right;
+        }
       } else {
         ret.push_back(cur->val);
-        cur = nullptr;
+        cur = cur->left;
       }
-
-      if (s.empty()) break;
     }
-
+    reverse(ret.begin(), ret.end());
     return ret;
   }
 };
