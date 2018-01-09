fefa873003a0f280e8d6dfe3f55a4a1f5173d4c4
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-09 14:24:27 -0600

    small bash script to see whether I've done before

---
+++ leetcode/query.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+todo=$(comm -13 \
+            <(ls *.cpp | sed -rn 's/([0-9]+)-.*$/\1/p' | sort) \
+            <(seq -w 761))
+
+res=$(echo $todo | tr ' ' '\n' | grep -xc "$1")
+
+if [ $res -eq 0 ]; then
+    echo -e "\033[01;32mFinished!\033[0m"
+else
+    echo -e "\033[01;31mNot Finished!\033[0m"
+fi
