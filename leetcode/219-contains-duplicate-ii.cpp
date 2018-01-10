13158ba8d348916f08ed707bcd22f561227b2a8c
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-10 14:42:52 -0600

    leetcode 219 (reformat)

--- leetcode/219-contains-duplicate-ii.cpp
+++ leetcode/219-contains-duplicate-ii.cpp
@@ -1,16 +1,14 @@
-class Solution {
-public:
-  bool containsNearbyDuplicate(vector<int>& nums, int k) {
+class Solution
+{
+ public:
+  bool containsNearbyDuplicate(vector<int>& nums, int k)
+  {
     unordered_map<int, int> m;
     for (int i = 0; i < nums.size(); ++i) {
       int n = nums[i];
-
-      if (m.find(n) != m.end())
-        if (i - m[n] <= k) return true;
-
+      if (m.find(n) != m.end() && i - m[n] <= k) return true;
       m[n] = i;
     }
-
     return false;
   }
 };
