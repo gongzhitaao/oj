1998b6b9f377e7c07ea265e11483863085c4ad19
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-09 19:40:05 -0600

    query script

--- leetcode/query.bash
+++ leetcode/query.bash
@@ -4,10 +4,14 @@ todo=$(comm -13 \
             <(ls *.cpp | sed -rn 's/([0-9]+)-.*$/\1/p' | sort) \
             <(seq -w 761))
 
-res=$(echo $todo | tr ' ' '\n' | grep -xc "$1")
-
-if [ $res -eq 0 ]; then
-    echo -e "\033[01;32mFinished!\033[0m"
+if [ $# -eq 0 ]; then
+    echo $todo
 else
-    echo -e "\033[01;31mNot Finished!\033[0m"
+    res=$(echo $todo | tr ' ' '\n' | grep -xc "$1")
+
+    if [ $res -eq 0 ]; then
+        echo -e "\033[01;32mFinished!\033[0m"
+    else
+        echo -e "\033[01;31mNot Finished!\033[0m"
+    fi
 fi
