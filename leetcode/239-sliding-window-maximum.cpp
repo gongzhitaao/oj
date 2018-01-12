cba95f0c1b614c6984f090cd2eb6d0bbe7737928
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-11 19:53:26 -0600

    leetcode 239 deque

--- leetcode/239-sliding-window-maximum.cpp
+++ leetcode/239-sliding-window-maximum.cpp
@@ -4,14 +4,16 @@ class Solution
   vector<int> maxSlidingWindow(vector<int>& nums, int k)
   {
     vector<int> ret;
-    deque<pair<int, int> > q;
+    deque<int> maxpos;
     for (int i = 0; i < nums.size(); ++i) {
-      while (!q.empty() && q.front().first <= nums[i])
-        q.pop_front();
-      q.push_front({nums[i], i});
-      if (i-q.back().second >= k)
-        q.pop_back();
-      if (i >= k-1) ret.push_back(q.back().first);
+      for (; !maxpos.empty() &&
+               (maxpos.back() <= i - k || nums[maxpos.back()] < nums[i]);
+           maxpos.pop_back())
+        ;
+      for (; !maxpos.empty() && maxpos.front() <= i - k; maxpos.pop_front())
+        ;
+      maxpos.push_back(i);
+      if (i >= k - 1) ret.push_back(nums[maxpos.front()]);
     }
     return ret;
   }
