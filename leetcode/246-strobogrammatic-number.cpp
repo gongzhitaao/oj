d37b00d3ae1a15f0df6e7fee2a8b703422de1bba
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-14 14:20:17 -0600

    leetcode 246 (reformat)

--- leetcode/246-strobogrammatic-number.cpp
+++ leetcode/246-strobogrammatic-number.cpp
@@ -5,11 +5,11 @@ class Solution
   {
     // 1 6 8 9 0
     int n = num.size();
-    for (int i = 0; i < (n+1)/2; ++i) {
+    for (int i = 0; i < (n + 1) / 2; ++i) {
       if (!((('0' == num[i] || '1' == num[i] || '8' == num[i]) &&
-             num[i] == num[n-i-1]) ||
-            ('6' == num[i] && '9' == num[n-i-1]) ||
-            ('9' == num[i] && '6' == num[n-i-1])))
+             num[i] == num[n - i - 1]) ||
+            ('6' == num[i] && '9' == num[n - i - 1]) ||
+            ('9' == num[i] && '6' == num[n - i - 1])))
         return false;
     }
     return true;
