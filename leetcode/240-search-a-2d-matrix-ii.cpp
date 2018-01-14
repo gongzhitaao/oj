4ee764651d2606eb79b9ca724f24fdbfda4d9ed1
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-14 10:18:45 -0600

    leetcode 240 linear search

--- leetcode/240-search-a-2d-matrix-ii.cpp
+++ leetcode/240-search-a-2d-matrix-ii.cpp
@@ -4,21 +4,15 @@ class Solution
   bool searchMatrix(vector<vector<int>>& matrix, int target)
   {
     if (matrix.empty() || matrix[0].empty()) return false;
-    return lookup(matrix, 0, matrix.size() - 1, 0, matrix[0].size() - 1,
-                  target);
-  }
 
-  bool lookup(vector<vector<int>>& mat, int ilo, int ihi, int jlo, int jhi,
-              int target)
-  {
-    if (ilo > ihi || jlo > jhi) return false;
-    int imi = ilo + (ihi - ilo) / 2, jmi = jlo + (jhi - jlo) / 2;
+    for (int i = 0, j = matrix[0].size() - 1; i < matrix.size() && j >= 0;) {
+      if (target == matrix[i][j]) return true;
+      if (target < matrix[i][j])
+        --j;
+      else
+        ++i;
+    }
 
-    if (mat[imi][jmi] == target) return true;
-    if (mat[imi][jmi] < target)
-      return lookup(mat, imi + 1, ihi, jlo, jhi, target) ||
-             lookup(mat, ilo, imi, jmi + 1, jhi, target);
-    return lookup(mat, ilo, imi - 1, jlo, jhi, target) ||
-           lookup(mat, imi, ihi, jlo, jmi - 1, target);
+    return false;
   }
 };
