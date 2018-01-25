9edd88139d4e8e33dac24e2f06cbb0d0f8e26bf3
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-25 13:36:16 -0600

    leetcode 255 stack

--- leetcode/255-verify-preorder-sequence-in-binary-search-tree.cpp
+++ leetcode/255-verify-preorder-sequence-in-binary-search-tree.cpp
@@ -3,21 +3,15 @@ class Solution
  public:
   bool verifyPreorder(vector<int>& preorder)
   {
-    int n = preorder.size();
-    return isvalid(preorder, 0, n - 1);
-  }
-
-  bool isvalid(vector<int>& vec, int lo, int hi)
-  {
-    if (hi - lo + 1 < 3) return true;
-
-    int mi = lo + 1;
-    for (; mi <= hi && vec[mi] < vec[lo]; ++mi)
-      ;
-
-    for (int i = mi; i <= hi; ++i)
-      if (vec[i] < vec[lo]) return false;
-
-    return isvalid(vec, lo + 1, mi - 1) && isvalid(vec, mi, hi);
+    int minval = numeric_limits<int>::min();
+    stack<int> left;
+    for (int cur : preorder) {
+      if (cur < minval)
+        return false;
+      for (; !left.empty() && left.top() < cur; left.pop())
+        minval = left.top();
+      left.push(cur);
+    }
+    return true;
   }
 };
