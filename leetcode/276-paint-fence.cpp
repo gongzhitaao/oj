d7acc264e33808ec8f48ed5d7318cff8dd46b2b4
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-29 13:58:07 -0600

    leetcode 276 dp

--- leetcode/276-paint-fence.cpp
+++ leetcode/276-paint-fence.cpp
@@ -4,10 +4,12 @@ class Solution
   int numWays(int n, int k)
   {
     if (0 == n) return 0;
-    vector<int> v(n+1, 0);
-    v[1] = k, v[2] = k*k, v[3] = k*k*k - k;
-    for (int i = 4; i <= n; ++i)
-      v[i] = v[i-1]*k - v[i-3]*(k-1);
-    return v[n];
+    int v0 = 0, v1 = k;
+    for (int i = 1; i < n; ++i) {
+      int t0 = v0;
+      v0 = v1;
+      v1 = (t0 + v1) * (k - 1);
+    }
+    return v0 + v1;
   }
 };
