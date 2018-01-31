cae9f71c7ef49f8f5723e12c31e5da879d4d05ce
Zhitao Gong <zhitaao.gong@gmail.com>
2018-01-30 20:13:49 -0600

    leetcode 287 binary search by counting

--- leetcode/287-find-the-duplicate-number.cpp
+++ leetcode/287-find-the-duplicate-number.cpp
@@ -3,15 +3,16 @@ class Solution
  public:
   int findDuplicate(vector<int>& nums)
   {
-    if (nums.size() < 2) return nums[0];
-
-    int slow = nums[0], fast = nums[nums[0]];
-    for (; slow != fast; slow = nums[slow], fast = nums[nums[fast]])
-      ;
-
-    for (slow = 0; slow != fast; slow = nums[slow], fast = nums[fast])
-      ;
-
-    return slow;
+    int lo = 1, hi = nums.size();
+    for (int mi; lo < hi; ) {
+      mi = lo + (hi - lo) / 2;
+      int cnt = 0;
+      for (int n : nums)
+        if (n <= mi) ++cnt;
+      if (cnt <= mi) lo = mi + 1;
+      else hi = mi;
+    }
+    return lo;
   }
+
 };
