90abbb1bb0f578d2c9f85e51e590d5acf61281b0
Zhitao Gong <zhitaao.gong@gmail.com>
2018-02-04 19:56:37 -0600

    leetcode 302 binary search

--- leetcode/302-smallest-rectangle-enclosing-black-pixels.cpp
+++ leetcode/302-smallest-rectangle-enclosing-black-pixels.cpp
@@ -3,28 +3,49 @@ class Solution
  public:
   int minArea(vector<vector<char>>& image, int x, int y)
   {
-    if (image.empty() || image[0].empty()) return 0;
-    int xmin = x, xmax = x;
-    int ymin = y, ymax = y;
-    dfs(image, x, y, xmin, ymin, xmax, ymax);
-    return (xmax - xmin + 1) * (ymax - ymin + 1);
+    const int row = image.size(), col = image[0].size();
+    int xmin = search_row(image, 0, x, 1);
+    int xmax = search_row(image, x, row, 0);
+    int ymin = search_col(image, 0, y, 1);
+    int ymax = search_col(image, y, col, 0);
+    return (xmax - xmin) * (ymax - ymin);
   }
 
-  void dfs(vector<vector<char>>& img, int x, int y, int& xmin, int& ymin,
-           int& xmax, int& ymax)
+  int search_row(vector<vector<char>>& img, int lo, int hi, int findmin)
   {
-    if (x < xmin) xmin = x;
-    else if (x > xmax) xmax = x;
-    if (y < ymin) ymin = y;
-    else if (y > ymax) ymax = y;
-    img[x][y] = 'x';
-    if (0 <= x - 1 && '1' == img[x - 1][y])
-      dfs(img, x - 1, y, xmin, ymin, xmax, ymax);
-    if (y + 1 < img[0].size() && '1' == img[x][y + 1])
-      dfs(img, x, y + 1, xmin, ymin, xmax, ymax);
-    if (x + 1 < img.size() && '1' == img[x + 1][y])
-      dfs(img, x + 1, y, xmin, ymin, xmax, ymax);
-    if (0 <= y - 1 && '1' == img[x][y - 1])
-      dfs(img, x, y - 1, xmin, ymin, xmax, ymax);
+    int col = img[0].size();
+    while (lo < hi) {
+      int mi = lo + (hi - lo) / 2;
+      int black = 0;
+      for (int i = 0; i < col; ++i)
+        if ('1' == img[mi][i]) {
+          black = 1;
+          break;
+        }
+      if (black ^ findmin)
+        lo = mi + 1;
+      else
+        hi = mi;
+    }
+    return lo;
+  }
+
+  int search_col(vector<vector<char>>& img, int lo, int hi, int findmin)
+  {
+    int row = img.size();
+    while (lo < hi) {
+      int mi = lo + (hi - lo) / 2;
+      int black = 0;
+      for (int i = 0; i < row; ++i)
+        if ('1' == img[i][mi]) {
+          black = 1;
+          break;
+        }
+      if (black ^ findmin)
+        lo = mi + 1;
+      else
+        hi = mi;
+    }
+    return lo;
   }
 };
