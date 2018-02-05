bcf642eff503e4ffc1d4234079fcfd51ddf3fc04
Zhitao Gong <zhitaao.gong@gmail.com>
2018-02-05 16:14:51 -0600

    leetcode 305 union find

--- leetcode/305-number-of-islands-ii.cpp
+++ leetcode/305-number-of-islands-ii.cpp
@@ -1,54 +1,53 @@
 class Solution
 {
- public:
-  typedef pair<int, int> id;
+  typedef pair<int, int> id_t;
 
-  vector<int> numIslands2(int m, int n, vector<id>& positions)
+ public:
+  vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions)
   {
     vector<int> ret;
-    if (!m || !n) return ret;
-
-    vector<vector<int>> G(m, vector<int>(n, 0));
-    vector<vector<id>> P(m, vector<id>(n, {-1, -1}));
-
-    const vector<int> step = {-1, 0, 1, 0, -1};
-    vector<int> counts;
+    if (positions.empty()) return ret;
+    vector<vector<id_t>> par(m, vector<id_t>(n, {-1, -1}));
+    vector<vector<int>> rnk(m, vector<int>(n, 0));
     int cnt = 0;
-    for (id pos : positions) {
-      int i = pos.first, j = pos.second;
-      G[i][j] = 1;
-      P[i][j] = {i, j};
+    vector<int> d{-1, 0, 1, 0, -1};
+    for (id_t p : positions) {
+      par[p.first][p.second] = p;
       ++cnt;
-      for (int k = 0; k < 4; ++k) {
-        int r = i + step[k], c = j + step[k + 1];
-        if (0 <= r && r < m && 0 <= c && c < n && G[r][c])
-          cnt -= myunion(P, G, P[i][j], P[r][c]);
+      for (int i = 0; i < 4; ++i) {
+        id_t nb = {p.first + d[i], p.second + d[i + 1]};
+        if (nb.first < 0 || nb.second < 0 || nb.first >= m || nb.second >= n ||
+            par[nb.first][nb.second].first < 0)
+          continue;
+        cnt -= Union(p, nb, par, rnk);
       }
-      counts.push_back(cnt);
+      ret.push_back(cnt);
     }
-    return counts;
+    return ret;
   }
 
-  int myunion(vector<vector<id>>& P, vector<vector<int>>& G, id a, id b)
+  id_t Find(id_t& p, vector<vector<id_t>>& par)
   {
-    id pa = findpar(P, a), pb = findpar(P, b);
+    if (par[p.first][p.second] != p)
+      par[p.first][p.second] = Find(par[p.first][p.second], par);
+    return par[p.first][p.second];
+  }
+
+  int Union(id_t& a, id_t& b, vector<vector<id_t>>& par,
+            vector<vector<int>>& rnk)
+  {
+    id_t pa = Find(a, par);
+    id_t pb = Find(b, par);
+    // They belong to the same island, so the total count does not decrease.
     if (pa == pb) return 0;
-
-    if (G[pa.first][pa.second] < G[pb.first][pb.second])
-      P[pa.first][pa.second] = pb;
-    else if (G[pa.first][pa.second] > G[pb.first][pb.second])
-      P[pb.first][pb.second] = pa;
+    if (rnk[pa.first][pa.second] < rnk[pb.first][pb.second])
+      par[pa.first][pa.second] = pb;
     else {
-      P[pb.first][pb.second] = pa;
-      ++G[pa.first][pa.second];
+      par[pb.first][pb.second] = pa;
+      if (rnk[pa.first][pa.second] == rnk[pb.first][pb.second])
+        ++rnk[pa.first][pa.second];
     }
+    // Now we merge two islands, so the total count should decrease by 1.
     return 1;
-  }
-
-  id findpar(vector<vector<id>>& P, id cur)
-  {
-    if (P[cur.first][cur.second] != cur)
-      P[cur.first][cur.second] = findpar(P, P[cur.first][cur.second]);
-    return P[cur.first][cur.second];
   }
 };
