7f5ea4d7a7b54411a46336d32eb8e4fb2b72d326
Zhitao Gong <zhitaao.gong@gmail.com>
2018-02-16 14:24:07 -0600

    leetcode 322 dp

--- leetcode/322-coin-change.cpp
+++ leetcode/322-coin-change.cpp
@@ -3,17 +3,14 @@ class Solution
  public:
   int coinChange(vector<int>& coins, int amount)
   {
-    if (coins.empty()) return -1;
+    sort(coins.begin(), coins.end());
     vector<int> cnt(amount + 1, -1);
     cnt[0] = 0;
-    for (int i = 0; i < coins.size(); ++i) {
-      for (int j = 1; j <= amount; ++j) {
-        int end = j / coins[i];
-        for (int k = 0; k <= end; ++k) {
-          int pre = cnt[j - k * coins[i]];
-          if (pre >= 0 && (cnt[j] < 0 || pre + k < cnt[j])) cnt[j] = pre + k;
-          if (cnt[j] >= 0 && cnt[j] < end) end = cnt[j];
-        }
+    for (int i = 1; i <= amount; ++i) {
+      for (int c : coins) {
+        if (c > i) break;
+        if (cnt[i - c] >= 0 && (cnt[i] < 0 || cnt[i - c] + 1 < cnt[i]))
+          cnt[i] = cnt[i - c] + 1;
       }
     }
     return cnt[amount];
