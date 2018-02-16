ab550f8269bdec299f1650c30c16b2e86419e25b
Zhitao Gong <zhitaao.gong@gmail.com>
2018-02-16 10:20:23 -0600

    leetcode 321 (reformat)

--- leetcode/320-generalized-abbreviation.cpp
+++ leetcode/320-generalized-abbreviation.cpp
@@ -10,10 +10,9 @@ class Solution
 
   void dfs(string word, string path, int n, vector<string>& ret)
   {
-    if (word.empty())
-      return ret.push_back(path + (n ? to_string(n) : ""));
+    if (word.empty()) return ret.push_back(path + (n ? to_string(n) : ""));
     string next = word.substr(1);
-    dfs(next, path, n+1, ret);
+    dfs(next, path, n + 1, ret);
     dfs(next, path + (n ? to_string(n) : "") + word[0], 0, ret);
   }
 };
