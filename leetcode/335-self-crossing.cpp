0679790505007a8ceccf449220d2b632e927394c
Zhitao Gong <zhitaao.gong@gmail.com>
2018-03-02 14:24:04 -0600

    leetcode 335 ad hoc

---
+++ leetcode/335-self-crossing.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+ public:
+  bool isSelfCrossing(vector<int>& x)
+  {
+    for (int i = 3; i < x.size(); ++i) {
+      if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) return true;
+      if (i > 3 && x[i - 1] == x[i - 3] && x[i] >= x[i - 2] - x[i - 4])
+        return true;
+      if (i > 4 && x[i - 2] >= x[i - 4] && x[i - 1] >= x[i - 3] - x[i - 5] &&
+          x[i] >= x[i - 2] - x[i - 4] && x[i - 1] <= x[i - 3])
+        return true;
+    }
+    return false;
+  }
+};
