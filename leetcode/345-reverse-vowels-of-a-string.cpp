752ab471d14cb1140fceaf9d3cf0ffab6f49b797
Zhitao Gong <zhitaao.gong@gmail.com>
2018-03-03 09:11:26 -0600

    leetcode 345 (reformat)

--- leetcode/345-reverse-vowels-of-a-string.cpp
+++ leetcode/345-reverse-vowels-of-a-string.cpp
@@ -3,10 +3,12 @@ class Solution
  public:
   string reverseVowels(string s)
   {
-    for (int i = 0, j = s.size() - 1; i < j; ) {
+    for (int i = 0, j = s.size() - 1; i < j;) {
       bool bi = isvowel(s[i]), bj = isvowel(s[j]);
-      if (bi && bj) swap(s[i++], s[j--]);
-      else i += !bi, j -= !bj;
+      if (bi && bj)
+        swap(s[i++], s[j--]);
+      else
+        i += !bi, j -= !bj;
     }
     return s;
   }
@@ -14,6 +16,6 @@ class Solution
   inline bool isvowel(char c)
   {
     return 'a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c ||
-        'A' == c || 'E' == c || 'I' == c || 'O' == c || 'U' == c;
+           'A' == c || 'E' == c || 'I' == c || 'O' == c || 'U' == c;
   }
 };
