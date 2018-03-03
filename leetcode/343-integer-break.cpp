f771b3c350d7691fe30401d4b79e7148d2f0a744
Zhitao Gong <zhitaao.gong@gmail.com>
2018-03-03 09:10:15 -0600

    leetcode 343 number theory

--- leetcode/343-integer-break.cpp
+++ leetcode/343-integer-break.cpp
@@ -3,16 +3,10 @@ class Solution
  public:
   int integerBreak(int n)
   {
-    vector<int> res(60, 0);
-    res[1] = res[2] = 1;
-    return dp(n, res);
-  }
-
-  int dp(int n, vector<int>& res)
-  {
-    if (res[n] > 0) return res[n];
-    for (int i = 1; i <= n / 2; ++i)
-      res[n] = max(max(i, dp(i, res)) * max(n - i, dp(n - i, res)), res[n]);
-    return res[n];
+    if (2 == n) return 1;
+    if (3 == n) return 2;
+    int ret = 1;
+    for (; n > 4; n -= 3) ret *= 3;
+    return ret * n;
   }
 };
