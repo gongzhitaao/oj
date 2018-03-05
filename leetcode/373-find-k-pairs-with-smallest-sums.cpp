e0f2b22ec35dd11fd3c21f0e00a1e4ea494bcd64
Zhitao Gong <zhitaao.gong@gmail.com>
2018-03-05 14:21:33 -0600

    leetcode 373 refactor, k smallest values in column/row sorted matrix

--- leetcode/373-find-k-pairs-with-smallest-sums.cpp
+++ leetcode/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,30 +1,25 @@
 class Solution
 {
  public:
-  vector<pair<int, int> >
-  kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k)
+  vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
+                                        int k)
   {
-    int n1 = nums1.size(), n2 = nums2.size();
-    vector<pair<int, int> > ret;
-    if (!n1 || !n2) return ret;
-
-    struct cmp {
-      bool operator()(tuple<int, int, int, int>& a,
-                      tuple<int, int, int, int>& b)
-      { return get<0>(a)+get<1>(a) > get<0>(b)+get<1>(b); }
+    if (nums1.empty() || nums2.empty()) return {};
+    typedef pair<int, int> pair_t;
+    const int n1 = nums1.size(), n2 = nums2.size();
+    auto cmp = [&nums1, &nums2](pair_t& a, pair_t& b) {
+      return nums1[a.first] + nums2[a.second] >
+             nums1[b.first] + nums2[b.second];
     };
-    priority_queue<tuple<int, int, int, int>,
-                   vector<tuple<int, int, int, int> >, cmp> q;
-
-    for (int i = 0; i < nums1.size(); ++i)
-      q.push(make_tuple(nums1[i], nums2[0], i, 0));
-
-    for (int i = 0; i < k && q.size(); ++i) {
-      auto t = q.top(); q.pop();
-      ret.push_back({get<0>(t), get<1>(t)});
-      if (get<3>(t)+1 < n2)
-        q.push(make_tuple(nums1[get<2>(t)], nums2[get<3>(t)+1],
-                          get<2>(t), get<3>(t)+1));
+    priority_queue<pair_t, vector<pair_t>, decltype(cmp)> q(cmp);
+    for (int i = 0; i < n1; ++i) q.push({i, 0});
+    vector<pair_t> ret;
+    int end = min(n1 * n2, k);
+    for (int i = 0; i < end; ++i) {
+      pair_t j = q.top();
+      q.pop();
+      ret.push_back({nums1[j.first], nums2[j.second]});
+      if (j.second + 1 < n2) q.push({j.first, j.second + 1});
     }
     return ret;
   }
